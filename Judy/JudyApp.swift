025edd96e76337a78c746dfef2181c563a65a12d
Sean You <officialhyunwoo3155@gmail.com>
2026-03-09 14:05:37 -0500

    Initial Commit

---
+++ Judy/JudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JudyApp.swift
+//  Judy
+//
+//  Created by 유현우 on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct JudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
